a90f7a9dab8f1e30c02a0b884d421ba2b862acfc
kjr <kjr0814@hotmail.com>
2021-11-20 14:01:09 +0900

    데이터 추출 => 이름만 ui가져와서 조합

--- app/src/main/java/com/neppplus/listview_20211120/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20211120/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.neppplus.listview_20211120.R
 import com.neppplus.listview_20211120.datas.StudentData
 
@@ -27,6 +28,16 @@ class StudentAdapter(
         }
 
         val row = tempRow!!
+
+//        row 실제 데이터 가공
+//        근더 데이터 가져오기
+        val data = mList[position]
+
+//        뿌려질 UI 가져오자
+        val txtStudentName = row.findViewById<TextView>(R.id.txtStudentName)
+
+        txtStudentName.text = data.name
+
 
         return row
 
